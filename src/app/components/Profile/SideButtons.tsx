a0dc2e823054ed4dbe30bc111f3ae3cd73bd09f9
ulziibadrakh.bb@gmail.com <24LP6749@pinecones-iMac.local>
2025-02-06 16:36:58 +0800

    addition

--- src/app/components/Profile/SideButtons.tsx
+++ src/app/components/Profile/SideButtons.tsx
@@ -5,17 +5,27 @@ import { useState } from "react";
 
 export function SideButtons() {
   const [showPersonalInfo, setShowPersonalInfo] = useState(false);
+  const [contactInfo, setContactInfo] = useState(false);
   return (
     <div className=" flex flex-col gap-1 ">
       <Button
-        onClick={() => setShowPersonalInfo((prev) => !prev)}
+        onClick={() => {
+          setShowPersonalInfo(!showPersonalInfo);
+        }}
         className="flex justify-start w-[250px] h-[36px] rounded-md px-4 py-2 gap-2 bg-white text-black shadow-none hover:bg-[#F4F4F5] text-sm font-medium leading-5 font-inter"
       >
         Personal Information
       </Button>
-      <Button className="flex justify-start w-[250px] h-[36px] rounded-md px-4 py-2 gap-2 bg-white text-black shadow-none hover:bg-[#F4F4F5] text-sm font-medium leading-5 font-inter">
+      <div>{showPersonalInfo && <PersonalInformation />}</div>
+      <Button
+        onClick={() => {
+          setContactInfo(!contactInfo);
+        }}
+        className="flex justify-start w-[250px] h-[36px] rounded-md px-4 py-2 gap-2 bg-white text-black shadow-none hover:bg-[#F4F4F5] text-sm font-medium leading-5 font-inter"
+      >
         Contact info
       </Button>
+      <div>{contactInfo && <>aa</>}</div>
       <Button className="flex justify-start w-[250px] h-[36px] rounded-md px-4 py-2 gap-2 bg-white text-black shadow-none hover:bg-[#F4F4F5] text-sm font-medium leading-5 font-inter">
         Security & Settings
       </Button>
